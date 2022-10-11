457f1fc7b995399a9f00ecd3ec63bf1634724dab
Morteza <mssoftco@yahoo.com>
2022-10-11 10:49:24 +0330

    add tailwind css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta charSet='utf-8' />
+        <title>Rick & Morty</title>
+        <meta name='description' content='Rick and Morty' />
+        <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no' />
+      </Head>
+  <Component {...pageProps} />
+      </>
+  );
 }
 
 export default MyApp;
